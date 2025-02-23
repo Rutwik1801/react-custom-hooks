8d6db0a3c8f44a33c3acc35181835208e183ab60
rutwik-kt <rutwik@kindtech.co.in>
2025-02-23 12:45:02 +0530

    improved useBroadcastChannel

--- src/hooks/useBroadcastChannel.tsx
+++ src/hooks/useBroadcastChannel.tsx
@@ -1,28 +1,46 @@
 import { useEffect, useRef, useState } from "react";
+import { useRefCallback } from "./useRefCallback";
 
-export const useBroadcastChannel = (channelName: string) => {
+interface BroadcastMessage<T = any> {
+  senderId?: string,
+  data: T,
+  timeStamp: number 
+}
+
+export const useBroadcastChannel = <T = any>(channelName: string, onMessage?: (message: BroadcastMessage<T>) => void) => {
   // Using ref to avoid rerenders
   const bcRef = useRef<BroadcastChannel | null>(null);
-  const [message, setMessage] = useState(null);
+  const [messages, setMessages] = useState<BroadcastMessage<T>[]>([]);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
 
-  const sendMessage = (data: any) => {
-    bcRef.current?.postMessage(data);
-  };
+  const sendMessage = useRefCallback((data: T, senderId?: string) => {
+    bcRef.current?.postMessage({data, timeStamp: Date.now(), senderId});
+  }, []);
 
   useEffect(() => {
     const bc = new BroadcastChannel(channelName);
     bcRef.current = bc;
+    setIsConnected(true);
 
-    bc.onmessage = (event: MessageEvent) => {
-      setMessage(event.data);
-    };
+    bc.onmessage = (e: MessageEvent<BroadcastMessage<T>>) => {
+      if(onMessage) {
+        onMessage(e.data);
+      }
+    setMessages(prev => [...prev, e.data])
+  }
 
+  bc.onmessageerror = () => {
+    console.log("bc Channel not connected")
+    setIsConnected(false)
+  }
     return () => {
       // Closing and clearing the event handler explicitly
+      bc.onmessageerror = null;
       bc.onmessage = null;
       bc.close();
+      setIsConnected(false);
     };
-  }, [channelName]);
+  }, [channelName, onMessage]);
 
-  return { sendMessage, message };
+  return { sendMessage, messages, isConnected };
 };
