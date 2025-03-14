af1e1fa44f825647f43de9f935a1e7ab254b160f
rutwik-kt <rutwik@kindtech.co.in>
2025-03-15 01:30:31 +0530

    added config for npm package

---
+++ react-beautiful-hooks/src/index.ts
@@ -0,0 +1,5 @@
+import { useBroadcastChannel } from "./hooks/useBroadcastChannel";
+import { useDebounce } from "./hooks/useDebounce";
+import { useRefCallback } from "./hooks/useRefCallback";
+
+export {useBroadcastChannel, useDebounce, useRefCallback};
